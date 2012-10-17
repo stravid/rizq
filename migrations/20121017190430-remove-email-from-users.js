d902410009aff2718871a34884b047c4f27992ed
Wolfgang Vogl <wuulf01@yahoo.de>
2012-10-17 22:33:35 +0200

    add migration to rename the name column from the users table into username

---
+++ migrations/20121017190430-remove-email-from-users.js
@@ -0,0 +1,10 @@
+var dbm = require('db-migrate');
+var type = dbm.dataType;
+
+exports.up = function(db, callback) {
+  db.removeColumn('users', 'email' , callback);
+};
+
+exports.down = function(db, callback) {
+  db.addColumn('users', 'email' , 'string' , callback);
+};
